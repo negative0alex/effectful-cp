4872ef7bc19bbe5b87196b0469ea5cbb8203efb1
Alex <at979@cam.ac.uk>
2025-08-26 16:16:51 +0300

    Optimised Staging

--- src/Staging/Optimised.hs
+++ src/Staging/Optimised.hs
@@ -1,43 +1,42 @@
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE StandaloneKindSignatures #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE GADTs #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Staging.Optimised where
 
 import Control.Monad.Free
+import Data.Kind
 import Effects.CPSolve
 import Effects.NonDet
 import Effects.Solver
+import Eval (SearchTree)
 import FD.OvertonFD
-import Transformers (flipT)
 import Language.Haskell.TH hiding (Type)
 import Queens ((/\))
 import Queues
 import Solver (Solver (..))
 import Staging.Old.Direct (rec2)
 import System.Random
+import Transformers (flipT)
 import Prelude hiding (fail)
-import Eval (SearchTree)
-import Data.Kind
 
 showCode :: Code Q a -> IO ()
 showCode code = do
@@ -45,27 +44,27 @@ showCode code = do
   putStrLn (pprint expr)
 
 data Rep :: Type -> Type where
-   Dyn  :: Code Q a -> Rep a   
-   Ret :: (Monad m) => Rep a -> Rep (m a)
-   Pair :: Rep a -> Rep b -> Rep (a,b)
+  Dyn :: Code Q a -> Rep a
+  Ret :: (Monad m) => Rep a -> Rep (m a)
+  Pair :: Rep a -> Rep b -> Rep (a, b)
 
-unRep :: Rep a -> Code Q a 
-unRep (Dyn a) = a 
-unRep (Ret a) = [|| pure $$(unRep a) ||]
-unRep (Pair a b) = [|| ($$(unRep a) , $$(unRep b)) ||]
+unRep :: Rep a -> Code Q a
+unRep (Dyn a) = a
+unRep (Ret a) = [||pure $$(unRep a)||]
+unRep (Pair a b) = [||($$(unRep a), $$(unRep b))||]
 
-pairR :: Rep a -> Rep b -> Rep (a,b)
+pairR :: Rep a -> Rep b -> Rep (a, b)
 pairR = Pair
 
-letR :: Rep (a,b) -> (Rep a -> Rep b -> Rep c) -> Rep c 
-letR (Pair a b) cont = cont a b
-letR (Dyn pair) cont = Dyn [|| let (a,b) = $$pair in $$(unRep $ cont (Dyn [|| a ||]) (Dyn [|| b ||]))||] 
+letR :: Rep (a, b) -> (ContRep (Rep a, Rep b))
+letR (Pair a b) = pure (a, b)
+letR (Dyn p) = CR $ \k -> Dyn [||let (a, b) = $$p in $$(unRep (curry k (Dyn [||a||]) (Dyn [||b||])))||]
 
 pureR :: (Monad m) => Rep a -> Rep (m a)
 pureR = Ret
 
 bindR :: (Monad m) => Rep (m a) -> (Rep a -> Rep (m b)) -> Rep (m b)
-bindR (Dyn ma) f = Dyn $ [|| $$ma >>= \a -> $$(unRep $ f (Dyn [|| a ||])) ||]
+bindR (Dyn ma) f = Dyn $ [||$$ma >>= \a -> $$(unRep $ f (Dyn [||a||]))||]
 bindR (Ret a) f = f a
 
 (>>=>) :: (Monad m) => Rep (m a) -> (Rep a -> Rep (m b)) -> Rep (m b)
@@ -73,29 +72,32 @@ bindR (Ret a) f = f a
 infixl 1 >>=>
 
 unitR :: Rep ()
-unitR = Dyn [|| () ||]
+unitR = Dyn [||()||]
 
-zeroR :: Rep Int 
-zeroR = Dyn [|| 0 ||]
+zeroR :: Rep Int
+zeroR = Dyn [||0||]
 
-newtype ContRep s = R {r :: forall b. (s -> Rep b) -> Rep b}
+newtype ContRep s = CR {cr :: forall b. (s -> Rep b) -> Rep b}
 
-instance Functor (ContRep) where 
+contR :: a -> ContRep a
+contR a = CR $ \cont -> cont a
+
+instance Functor (ContRep) where
   fmap :: (a -> b) -> ContRep a -> ContRep b
-  fmap f (R ka) = R $ \kb -> ka $ \a -> kb (f a)
+  fmap f (CR ka) = CR $ \kb -> ka $ \a -> kb (f a)
 
-instance Applicative (ContRep) where 
+instance Applicative (ContRep) where
   pure :: a -> ContRep a
-  pure a = R $ (flip ($) a)
+  pure a = CR $ (flip ($) a)
 
   (<*>) :: ContRep (a -> b) -> ContRep a -> ContRep b
-  (R kf) <*> (R ka) = R $ \kb -> kf $ \f -> ka $ \a -> kb (f a)
+  (CR kf) <*> (CR ka) = CR $ \kb -> kf $ \f -> ka $ \a -> kb (f a)
 
-instance Monad (ContRep) where 
+instance Monad (ContRep) where
   (>>=) :: ContRep a -> (a -> ContRep b) -> ContRep b
-  (R kf) >>= f = R $ \k -> kf $ \a -> r (f a) k
+  (CR kf) >>= f = CR $ \k -> kf $ \a -> cr (f a) k
 
-newtype StateTransform state solver = ST {unST :: (Rep state -> Rep (solver state))}
+newtype StateTransform state solver = ST {unST :: (Rep state -> ContRep (Rep (solver state)))}
 
 newtype NextTransform ts es solver a
   = NT
@@ -103,21 +105,21 @@ newtype NextTransform ts es solver a
       Rep ts ->
       Rep es ->
       Rep (SearchTree solver a) ->
-      (Rep ts, Rep es, Rep (SearchTree solver a))
+      ContRep (Rep ts, Rep es, Rep (SearchTree solver a))
   }
 
 idState :: (Solver solver) => StateTransform state solver
-idState = ST $ pureR
+idState = ST $ contR . pureR
 
-liftST :: (Monad solver) => (Code Q a -> Code Q a) -> StateTransform a solver 
-liftST f = ST $ \ra -> pureR . Dyn $ f (unRep ra)
+liftST :: (Monad solver) => (Code Q a -> Code Q a) -> StateTransform a solver
+liftST f = ST $ \ra -> contR . pureR . Dyn $ f (unRep ra)
 
-crossST :: Monad solver => StateTransform s1 solver -> StateTransform s2 solver -> StateTransform (s1, s2) solver 
-crossST st1 st2 = ST $ \s -> letR s $ \s1 s2 -> 
-          (unST $ st1) s1 >>=> \s1' -> 
-            (unST $ st2) s2 >>=> \s2' -> 
-              pureR $ pairR s1' s2'
-
+crossST :: (Monad solver) => StateTransform s1 solver -> StateTransform s2 solver -> StateTransform (s1, s2) solver
+crossST st1 st2 = ST $ \s -> do
+  (s1, s2) <- letR s
+  s1' <- unST st1 $ s1
+  s2' <- unST st2 $ s2
+  pure $ s1' >>=> \s1'' -> s2' >>=> \s2'' -> pureR $ pairR s1'' s2''
 
 data (Solver solver) => SearchTransformer ts es solver a = SearchTransformer
   { tsInit :: Rep ts
@@ -134,9 +136,9 @@ dbsTrans depthLimit =
     { tsInit = zeroR
     , esInit = unitR
     , solEs = idState
-    , leftTs = liftST $ \a -> [|| $$a + 1 ||]
-    , rightTs = liftST $ \a -> [|| $$a + 1 ||]
-    , nextState = NT $ \ts es model -> (ts, es, Dyn [||if $$(unRep ts) <= depthLimit then $$(unRep model) else fail||])
+    , leftTs = liftST $ \a -> [||$$a + 1||]
+    , rightTs = liftST $ \a -> [||$$a + 1||]
+    , nextState = NT $ \ts es model -> pure (ts, es, Dyn [||if $$(unRep ts) <= depthLimit then $$(unRep model) else fail||])
     }
 
 -- branch and bound scaffolding
@@ -155,37 +157,42 @@ bbNB ::
 bbNB newBound =
   SearchTransformer
     { tsInit = zeroR
-    , esInit = Dyn [|| BBP 0 id ||]
-    , solEs = 
-      ST $ \es -> newBound >>=> \bound -> pureR $ Dyn [|| let (BBP v _) = $$(unRep es) in BBP (v+1) $$(unRep bound)||]
+    , esInit = Dyn [||BBP 0 id||]
+    , solEs =
+        ST $ \es ->
+          pure $ newBound >>=> \bound -> pureR $ Dyn [||let (BBP v _) = $$(unRep es) in BBP (v + 1) $$(unRep bound)||]
     , leftTs = idState
     , rightTs = idState
     , nextState = NT @Int @(BBEvalState solver a) @solver $ \ts es tree ->
-        ( Dyn [||
-          let (BBP nv _) = $$(unRep es)
-              v = $$(unRep ts)
-           in if nv > v then nv else v
-          ||]
-        , es
-        , Dyn [||
-          let (BBP nv bound) = $$(unRep es)
-              v = $$(unRep ts)
-           in if nv > v
-                then bound $$(unRep tree)
-                else $$(unRep tree)
-          ||]
-        )
+        pure
+          ( Dyn
+              [||
+              let (BBP nv _) = $$(unRep es)
+                  v = $$(unRep ts)
+               in if nv > v then nv else v
+              ||]
+          , es
+          , Dyn
+              [||
+              let (BBP nv bound) = $$(unRep es)
+                  v = $$(unRep ts)
+               in if nv > v
+                    then bound $$(unRep tree)
+                    else $$(unRep tree)
+              ||]
+          )
     }
 
 newBound :: forall a. NewBound OvertonFD a
 newBound =
-  Dyn [||
-  do
-    obj <- fd_objective
-    dom <- fd_domain $ obj
-    let val = head dom
-    return ((\tree -> obj @< val /\ tree))
-  ||]
+  Dyn
+    [||
+    do
+      obj <- fd_objective
+      dom <- fd_domain $ obj
+      let val = head dom
+      return ((\tree -> obj @< val /\ tree))
+    ||]
 
 bbS :: SearchTransformer Int (BBEvalState OvertonFD a) OvertonFD a
 bbS = bbNB newBound
@@ -197,27 +204,29 @@ ldsS discLimit =
     , esInit = unitR
     , solEs = idState
     , leftTs = idState
-    , rightTs = liftST $ \a -> [|| $$a + 1 ||]
+    , rightTs = liftST $ \a -> [||$$a + 1||]
     , nextState = NT $ \disc u tree ->
-        ( disc
-        , u
-        , Dyn [||if $$(unRep disc) <= discLimit then $$(unRep tree) else fail||]
-        )
+        pure
+          ( disc
+          , u
+          , Dyn [||if $$(unRep disc) <= discLimit then $$(unRep tree) else fail||]
+          )
     }
 
 randS :: (Solver solver) => Int -> SearchTransformer () [Bool] solver a
 randS seed =
   SearchTransformer
     { tsInit = unitR
-    , esInit = Dyn [|| randoms $ mkStdGen seed ||]
+    , esInit = Dyn [||randoms $ mkStdGen seed||]
     , solEs = idState
     , leftTs = idState
     , rightTs = idState
     , nextState = NT $ \u coins tree ->
-        ( u
-        , Dyn [||tail $$(unRep coins)||]
-        , Dyn [||let tree' = $$(unRep tree) in if head $$(unRep coins) then flipT tree' else tree'||]
-        )
+        pure
+          ( u
+          , Dyn [||tail $$(unRep coins)||]
+          , Dyn [||let tree' = $$(unRep tree) in if head $$(unRep coins) then flipT tree' else tree'||]
+          )
     }
 
 stage ::
@@ -233,14 +242,32 @@ stage (SearchTransformer tsInit esInit leftTs rightTs solEs nextState) =
     ( \(go, continue) ->
         [||
         \ts es q tree -> case tree of
-          Pure a -> do
-            es' <- $$(unRep $ unST solEs $ Dyn [||es||])
-            (a :) <$> ($$continue es' q)
-          l :|: r -> do
-            now <- mark
-            tsL <- $$(unRep $ unST leftTs $ Dyn [||ts||])
-            tsR <- $$(unRep $ unST rightTs $ Dyn [||ts||])
-            $$continue es (pushQ (now, tsL, l) $ pushQ (now, tsR, r) q)
+          Pure a ->
+            $$( unRep $
+                  ( cr
+                      (unST solEs (Dyn [||es||]))
+                      (>>=> \es' -> Dyn [||(a :) <$> ($$continue $$(unRep es') q)||])
+                  )
+              )
+          l :|: r ->
+            $$( unRep $
+                  ( cr
+                      (unST leftTs (Dyn [||ts||]))
+                      $ \tsL ->
+                        cr
+                          (unST rightTs (Dyn [||ts||]))
+                          $ \tsR ->
+                            Dyn [||mark||] >>=> \now ->
+                              tsL >>=> \tsL' ->
+                                tsR >>=> \tsR' ->
+                                  Dyn
+                                    [||
+                                    $$continue es $
+                                      pushQ ($$(unRep now), $$(unRep tsL'), l) $
+                                        pushQ ($$(unRep now), $$(unRep tsR'), r) q
+                                    ||]
+                  )
+              )
           Fail -> $$continue es q
           (Add c k) -> do
             success <- addCons c
@@ -261,8 +288,12 @@ stage (SearchTransformer tsInit esInit leftTs rightTs solEs nextState) =
             then pure []
             else
               let ((label, ts, tree), q') = popQ q
-               in $$( let (ts', es', tree') = unNT nextState (Dyn [||ts||]) (Dyn [||es||]) (Dyn [||tree||])
-                       in [||$$go $$(unRep ts') $$(unRep es') q' ((solve $ goto label) >> $$(unRep tree'))||]
+               in $$( unRep $
+                        cr
+                          (unNT nextState (Dyn [||ts||]) (Dyn [||es||]) (Dyn [||tree||]))
+                          ( \(ts', es', tree') ->
+                              Dyn [||$$go $$(unRep ts') $$(unRep es') q' ((solve $ goto label) >> $$(unRep tree'))||]
+                          )
                     )
         ||]
     )
@@ -280,14 +311,12 @@ composeTrans t1 t2 =
     , leftTs = crossST (leftTs t1) (leftTs t2)
     , rightTs = crossST (rightTs t1) (rightTs t2)
     , solEs = crossST (solEs t1) (solEs t2)
-    , nextState = NT $ \ts es tree -> undefined
-        -- let ts1 = [||fst $$ts||]
-        --     ts2 = [||snd $$ts||]
-        --     es1 = [||fst $$es||]
-        --     es2 = [||snd $$es||]
-        --     (ts1', es1', tree') = unNT (nextState t1) ts1 es1 tree
-        --     (ts2', es2', tree'') = unNT (nextState t2) ts2 es2 tree'
-        --  in ([||($$ts1', $$ts2')||], [||($$es1', $$es2')||], tree'')
+    , nextState = NT $ \ts es tree -> do
+        (ts1, ts2) <- letR ts
+        (es1, es2) <- letR es
+        (ts1', es1', tree') <- (unNT $ nextState t1) ts1 es1 tree
+        (ts2', es2', tree'') <- (unNT $ nextState t2) ts2 es2 tree'
+        pure (pairR ts1' ts2', pairR es1' es2', tree'')
     }
 
 (%&) ::
@@ -304,7 +333,9 @@ bbLdsRandS :: Int -> Int -> SearchTransformer (((), (Int, Int))) (([Bool], ((), 
 bbLdsRandS seed discrepancy = (randS seed) %& (ldsS discrepancy) %& bbS
 
 bbLdsRandCode ::
-  Int -> Int -> Code
+  Int ->
+  Int ->
+  Code
     Q
     ( [(Label OvertonFD, ((), (Int, Int)), SearchTree OvertonFD a)] ->
       SearchTree OvertonFD a ->
